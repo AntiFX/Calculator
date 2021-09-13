3a09629fb8026c3a0d68a697a773a4153c3ac8e3
Tyler <tyler.hagstrom@gmail.com>
2021-09-13 09:57:57 -0500

    added a working operator function

--- calc.js
+++ calc.js
@@ -1,15 +1,23 @@
-function add(num1, num2){
-    return num1 + num2;
-}
+function add(num1, num2){return num1 + num2;}
 
-function subtract(num1, num2){
-    return num1 - num2;
-}
+function subtract(num1, num2){return num1 - num2;}
 
-function multiply(num1, num2){
-    return num1 * num2;
-}
+function multiply(num1, num2){return num1 * num2;}
 
-function divide(num1, num2){
-    return num1 / num2;
+function divide(num1, num2){return num1 / num2;}
+
+function operator(num1, num2, oper){
+    switch(oper){
+        case '+':
+            return add(num1,num2);
+            break;
+        case '-':
+            return subtract(num1,num2);
+            break;
+        case '*':
+            return multiply(num1,num2);
+            break;
+        case '/':
+            return divide(num1,num2);
+    }
 }
